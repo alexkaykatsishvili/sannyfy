2abdfe7be98053028bb86de8c01089807d264c24
Alex Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2018-07-24 16:51:11 +0300

    Added new features for my sannyfy app

--- src/App.js
+++ src/App.js
@@ -5,13 +5,36 @@ import './App.css';
 let textColor = '#ccc';
 let defaultStyle = {
   color: textColor
+};
+let fakeServerData = {
+  user: {
+    name: 'Alex',
+    playlists: [
+      {
+        name: 'My favorites',
+        songs: ['Beat it', 'Supermassive Black Hole', 'Starway to Heaven']
+      },
+      {
+        name: 'Weekend playlist',
+        songs: ['Smooth', 'Give away', 'Numb']
+      },
+      {
+        name: 'Work playlist',
+        songs: ['Heavy road', 'The office', 'Funky town']
+      },
+      {
+        name: 'Joga playlist',
+        songs: ['Relax limo', 'Vinnyk\'s love', 'What ya looking at?']
+      },
+    ]
+  }
 }
 
 class Aggregate extends Component {
   render() {
     return (
       <div style={{width: '40%', display: 'inline-block'}}>
-        <h2 style={{color: textColor}}>Number text</h2>
+        <h2 style={{color: textColor}}>{this.props.playlists && this.props.playlists.length} playlists</h2>
       </div>
     );
   }
@@ -45,18 +68,36 @@ class Playlist extends Component {
 }
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {serverData : {}};
+  }
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({serverData : fakeServerData});
+    }, 1000);
+  }
   render() {
-    let title = 'Sannyfy';
     return (
       <div className="App">
-        <h1>{title}</h1>
-        <Aggregate/>
-        <Aggregate/>
-        <Filter/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
-        <Playlist/>
+        {this.state.serverData.user ?
+          <div>
+            <h1>{this.state.serverData.user.name}'s playlist</h1>
+            <Aggregate playlists={this.state.serverData.user.playlists}/>
+            <Aggregate/>
+            <Filter/>
+            <Playlist/>
+            <Playlist/>
+            <Playlist/>
+            <Playlist/>
+          </div> :
+          <div>
+            <h1>Loading...</h1>
+            <div className="App">
+              <img className="App-logo" src={logo}/>
+            </div>
+          </div>
+        } 
       </div>
     );
   }
