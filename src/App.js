7df6b47cfff08fe2f16c0eb0a0c6cf1094d128d3
Alex Kaikatsishvili <alexander.kaykatsishvili@gmail.com>
2018-07-25 11:04:37 +0300

    Dinamic playlist

--- src/App.js
+++ src/App.js
@@ -109,14 +109,17 @@ class Filter extends Component {
 
 class Playlist extends Component {
   render() {
+    let playlist = this.props.playlist;
     return (
       <div style={{display: 'inline-block', width: '25%'}}>
         <img/>
-        <h3>Playlist name</h3>
+        <h3>{playlist.name}</h3>
         <ul style={{...defaultStyle}}>
-          <li>Song 1</li>
-          <li>Song 2</li>
-          <li>Song 3</li>
+          {
+            playlist.songs.map(song =>
+              <li>{song.name}</li>
+            )
+          }
         </ul>
       </div>
     );
@@ -142,10 +145,11 @@ class App extends Component {
             <PlaylistCounter playlists={this.state.serverData.user.playlists}/>
             <HoursCounter playlists={this.state.serverData.user.playlists}/>
             <Filter/>
-            <Playlist/>
-            <Playlist/>
-            <Playlist/>
-            <Playlist/>
+            {
+              this.state.serverData.user.playlists.map(playlist =>
+                <Playlist playlist={playlist}/>
+              )
+            }
           </div> :
           <div>
             <h1>Loading...</h1>
